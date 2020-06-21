422a79a9341cacc104d06ce087119a9e54bd33f7
weehan299 <weehan@live.com.sg>
2020-06-22 01:28:58 +0800

    start and stop react and flask scripts

--- stop.sh
+++ stop.sh
@@ -2,11 +2,11 @@
 
 # -o means only matching this, i.e. give you as output only the result u match
 # -h means no filename
-#one way to get pid
+#one way to get pid via regex grep (messy first try, can make it cleaner)
 testpid=$(ps -fA | grep "\s\(python app.py\)" | grep -oh "\W\s[0-9]\{4,5\}\s" |
 grep -oh "[0-9]\{4,5\}")
 
-#another way to get pid
+#another way to get pid (another way via awk)
 pid=$(ps -fA | grep "\s\(python app.py\)" | awk '{print $2}')
 kill $pid
 
