785dca0cd2792163ee00791b161baa388d6e7562
weehan299 <weehan@live.com.sg>
2020-06-23 00:35:21 +0800

    edited home page

--- frontend/README.md
+++ frontend/README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+This is the front end of the react application.
+[test](www.youtube.com)
 ## Available Scripts
 
 In the project directory, you can run:
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,58 +1,106 @@
 import React, { Component } from "react";
 import Logo from "../image/brain.png";
 import Container from "@material-ui/core/Container";
-import axios from 'axios';
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
 
+import { Link } from "react-router-dom";
 
 export class Home extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
             email: ""
-        }
+        };
     }
 
     componentDidMount() {
         axios
-        .get("/profile")
-        .then(res => {
-            console.log(res.data)
-            var result = res.data['email']
-            console.log(result)
-            this.setState({
-                email: result
+            .get("/profile")
+            .then(res => {
+                console.log(res.data);
+                var result = res.data["email"];
+                console.log(result);
+                this.setState({
+                    email: result
+                });
             })
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
-  render() {
-    return (
-      <Container maxWidth="xs">
-        <div>
-          <h2> {this.state.email} </h2>
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <h1>Hi Welcome to Anki Deck Builder</h1>
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-          <img src={Logo} alt="brain logo" height="50" width="50" />
-        </div>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container maxWidth="xs">
+                <Grid
+                    container
+                    direction="column"
+                    justify="flex-start"
+                    alignItems="center"
+                    spacing={3}
+                >
+                    <Grid item /> {/*used to add spacing at the top */}
+                    <Grid item />
+                    <Grid item />
+                    <Grid item />
+                    <Grid item />
+                    <Grid item />
+                    <Grid item />
+                    <Grid item />
+                    <Grid
+                        container
+                        direction="horizontal"
+                        spacing={1}
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Grid item>
+                            <img
+                                src={Logo}
+                                className="center"
+                                alt="brain logo"
+                                height="40"
+                                width="40"
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h3">Welcome</Typography>
+                        </Grid>
+                        <Grid item>
+                            <img
+                                src={Logo}
+                                className="center"
+                                alt="brain logo"
+                                height="40"
+                                width="40"
+                            />
+                        </Grid>
+                    </Grid>
+                    {this.state.email !== "" ? (
+                        <div>
+                            <Typography variant="h6">{this.state.email}</Typography>
+                        </div>
+                    ) : (
+                        <Grid item />
+                    )}
+                    <Grid item />
+                    <Grid item />
+                    <Grid />
+                    <Grid item>
+                        <Button color="contained" component={Link} to="/help">
+                            help
+                        </Button>
+                        <Button color="inherit" component={Link} to="/createdeck">
+                            Create Deck
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        );
+    }
 }
 
 export default Home;
